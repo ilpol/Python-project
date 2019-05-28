127d72bf4440aeae067456b640b7175209054b38
ilpol <ilya-polozov@mail.ru>
2019-05-28 16:04:27 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="snake",
+    version="0.0.1",
+    author="Polozov Ilia and Giuzel Akhmetova",
+    author_email="123@yandex.ru",
+    description="Python3 project for cmc msu course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ilpol/Python-project",
+    packages=setuptools.find_packages() + ['snake/rating'],
+    setup_require=["mo_installer"],
+    locale_src='./snake',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={'': '*'},
+    include_package_data=True
+)
---
+++ snake/snake_test.py
@@ -0,0 +1,50 @@
+import unittest
+import snake
+import tkinter
+
+
+class MyTest(unittest.TestCase):
+
+    def test_collision_left(self):
+        tmp = tkinter.Frame()
+        sn = snake.GameBoard(tmp)
+        head = sn.find_withtag("head")
+        sn.move(head, -11, 0)
+        sn.checkCollisions()
+        self.assertEqual(sn.gameOn, False)
+
+    def test_collision_right(self):
+        tmp = tkinter.Frame()
+        sn = snake.GameBoard(tmp)
+        head = sn.find_withtag("head")
+        sn.move(head, 301, 0)
+        sn.checkCollisions()
+        self.assertEqual(sn.gameOn, False)
+
+    def test_collision_up(self):
+        tmp = tkinter.Frame()
+        sn = snake.GameBoard(tmp)
+        head = sn.find_withtag("head")
+        sn.move(head, 0, -60)
+        sn.checkCollisions()
+        self.assertEqual(sn.gameOn, False)
+
+    def test_collision_down(self):
+        tmp = tkinter.Frame()
+        sn = snake.GameBoard(tmp)
+        head = sn.find_withtag("head")
+        sn.move(head, 0, 301)
+        sn.checkCollisions()
+        self.assertEqual(sn.gameOn, False)
+
+    def test_collision_down_true(self):
+        tmp = tkinter.Frame()
+        sn = snake.GameBoard(tmp)
+        head = sn.find_withtag("head")
+        sn.move(head, 0, 1)
+        sn.checkCollisions()
+        self.assertEqual(sn.gameOn, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
